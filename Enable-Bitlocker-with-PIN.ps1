bd4b97259bab3ecf9b01d2ed1317e91b8d955056
Katy Nicholson <62079949+CoasterKaty@users.noreply.github.com>
2022-09-22 08:43:37 +0100

    Update Enable-Bitlocker-with-PIN.ps1

--- Enable-Bitlocker-with-PIN.ps1
+++ Enable-Bitlocker-with-PIN.ps1
@@ -1,10 +1,9 @@
 $OSVolume = Get-BitlockerVolume | Where {$_.VolumeType -eq "OperatingSystem"}
 if ($OSVolume.VolumeStatus -eq "FullyDecrypted") {
-  $DeviceSerial = (Get-WmiObject win32_bios).Serialnumber
+  $DeviceSerial = ((Get-WmiObject win32_bios).Serialnumber).ToUpper()
   $DevicePIN = ConvertTo-SecureString $DeviceSerial -AsPlainText -Force
   Add-BitlockerKeyProtector -MountPoint $OSVolume.MountPoint -RecoveryPasswordProtector
   Enable-BitLocker -MountPoint $OSVolume.MountPoint -Pin $DevicePIN -TPMandPinProtector
   $RecoveryKeyID = ((Get-BitlockerVolume).KeyProtector | Where {$_.KeyProtectorType -eq "RecoveryPassword"}).KeyProtectorId
   BackupToAAD-BitlockerKeyProtector -MountPoint $OSVolume.MountPoint -KeyProtectorId $RecoveryKeyID
-  shutdown.exe /r /f /t 60
 }
